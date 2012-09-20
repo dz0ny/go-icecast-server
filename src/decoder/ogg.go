203fcffbd547c399d2a7edee2b387b6d26bde93a
Janez Troha <janez.troha@gmail.com>
2012-09-20 12:16:51 +0200

    Export ogg packet, optimize converter

--- src/decoder/ogg.go
+++ src/decoder/ogg.go
@@ -2,6 +2,7 @@ package ogg
 
 import (
 	"bytes"
+	"encoding/binary"
 	"errors"
 	"log"
 	"utils"
@@ -13,14 +14,13 @@ const (
 
 //ogg v1 packet structure see http://xiph.org/ogg/doc/framing.html
 type Packet struct {
-	stream_structure_version  uint32
-	header_type_flag          uint32
-	absolute_granule_position uint64
-	stream_serial_number      uint32
-	page_sequence_no          uint32
-	page_checksum             uint32
-	page_segments             uint32
-	segment_table             uint32
+	Stream_structure_version  uint8
+	Header_type_flag          uint8
+	Absolute_granule_position uint64
+	Stream_serial_number      uint32
+	Page_sequence_no          uint32
+	Page_checksum             uint32
+	Page_segments             uint8
 	Info                      *Meta
 }
 
@@ -55,29 +55,34 @@ func parseForComments(data []byte, meta *Meta) {
 
 }
 
+func FindOgg(data []byte) int {
+	return bytes.Index(data[0:], []byte(OggHeader))
+}
+
 //returns new parsed ogg packet or err if it's not ogg packet
 func NewOggpacket(ogg []byte, startAddress int) (Packet, error, int) {
 	packet := new(Packet)
 	var headerLoc int
+
 	if startAddress > 0 {
 		headerLoc = startAddress
 	} else {
-		headerLoc = bytes.Index(ogg[0:], []byte(OggHeader))
+		headerLoc = FindOgg(ogg[0:])
 	}
 
 	if headerLoc == -1 {
 		return *packet, errors.New("Missing ogg header in bitstream"), -1
 	}
 
-	packet.stream_structure_version = Varint32(ogg[headerLoc+4 : 5+headerLoc])
-	packet.header_type_flag = Varint32(ogg[headerLoc+5 : 6+headerLoc])
-	packet.absolute_granule_position = Varint64(ogg[headerLoc+6 : 14+headerLoc])
-	packet.stream_serial_number = Varint32(ogg[headerLoc+14 : 18+headerLoc])
-	packet.page_sequence_no = Varint32(ogg[headerLoc+18 : 22+headerLoc])
-	packet.page_checksum = Varint32(ogg[headerLoc+22 : 26+headerLoc])
-	packet.page_segments = Varint32(ogg[headerLoc+26 : 27+headerLoc])
+	packet.Stream_structure_version = uint8(ogg[headerLoc+4])
+	packet.Header_type_flag = uint8(ogg[headerLoc+5])
+	packet.Absolute_granule_position = Varint64(ogg[headerLoc+6 : 14+headerLoc])
+	packet.Stream_serial_number = Varint32(ogg[headerLoc+14 : 18+headerLoc])
+	packet.Page_sequence_no = Varint32(ogg[headerLoc+18 : 22+headerLoc])
+	packet.Page_checksum = Varint32(ogg[headerLoc+22 : 26+headerLoc])
+	packet.Page_segments = uint8(ogg[headerLoc+26])
 
-	if packet.header_type_flag != 0 {
+	if packet.Header_type_flag != 0 {
 
 		meta := new(Meta)
 		parseForComments(ogg[27:], meta)
@@ -87,33 +92,24 @@ func NewOggpacket(ogg []byte, startAddress int) (Packet, error, int) {
 			packet.Info = meta
 		}
 	}
-	//find next and return position
-	headerLoc = bytes.Index(ogg[27+headerLoc:], []byte(OggHeader))
+
 	return *packet, nil, headerLoc
 }
 
 // convert []bytes to uint32
 func Varint32(slice []byte) uint32 {
-	number := uint32(slice[0])
-	shift := uint(8)
-
-	for i := 1; i < len(slice); i++ {
-
-		number |= uint32(slice[i]) << shift
-		shift *= 2
+	l := len(slice)
+	if l == 1 {
+		return uint32(slice[0])
 	}
-	return number
+	return binary.LittleEndian.Uint32(slice[0:])
 }
 
 // convert []bytes to uint32
 func Varint64(slice []byte) uint64 {
-	number := uint64(slice[0])
-	shift := uint(8)
-
-	for i := 1; i < len(slice); i++ {
-
-		number |= uint64(slice[i]) << shift
-		shift *= 2
+	l := len(slice)
+	if l == 1 {
+		return uint64(slice[0])
 	}
-	return number
+	return binary.LittleEndian.Uint64(slice[0:])
 }
